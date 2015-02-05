b30f91c411f512ab4bcb32d186ae318baa7e22ae
bahuma20 <bahuma20@bahuma-online.de>
2015-02-05 14:53:57 +0100

    Added voice-elements

--- js/angelina-face.js
+++ js/angelina-face.js
@@ -2,6 +2,29 @@ jQuery(document).ready(function($) {
 	var changeAction = function(action) {
 		$('#face').removeClass().addClass('action').addClass(action);
 
+		switch (action) {
+			case 'question': 
+				document.querySelector('#mouth').setAttribute("text", "Wie lautet dein Passwort?");
+				window.setTimeout(function() {
+					document.querySelector('#mouth').speak();
+				}, 100);
+				break;
+
+			case 'error': 
+				document.querySelector('#mouth').setAttribute("text", "Das Passwort ist falsch!");
+				window.setTimeout(function() {
+					document.querySelector('#mouth').speak();
+				}, 100);
+				break;
+
+			case 'ok': 
+				document.querySelector('#mouth').setAttribute("text", "Das Passwort ist richtig!");
+				window.setTimeout(function() {
+					document.querySelector('#mouth').speak();
+				}, 100);
+				break;
+		}
+
 		window.setTimeout(function() {
 			$('#face').removeClass('action');
 		}, 1000);
